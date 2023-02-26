005f7e0f83ba3f18c3d442a1452eba99acd4ff34
Lucas Pacheco Teixeira <lucas@eliti.com.br>
2023-02-25 22:17:16 -0300

    Update rows.tsx

--- components/metanoia/states/rows.tsx
+++ components/metanoia/states/rows.tsx
@@ -1,11 +1,66 @@
 const state = {
   god: true,
   life: true,
-  row0: "Deus",
-  row1: "Eu",
-  row2: "Outro",
-  row3: "Mente",
-  row4: "Ações",
+  row0: (
+    <Row
+      number={0}
+      element=""
+      title="Deus / Poder"
+      description="No que confio?"
+    />
+  ),
+  row1: (
+    <Row
+      number={1}
+      element="FOGO"
+      title="Eu / Indivíduo"
+      description="Quem sou eu?"
+    />
+  ),
+  row2: (
+    <Row
+      number={2}
+      element="ÁGUA"
+      title="Ego / Relações"
+      description="Quem são os outros?"
+    />
+  ),
+  row3: (
+    <Row
+      number={3}
+      element="AR"
+      title="Mente / Inteligência"
+      description="Qual o propósito disso tudo?"
+    />
+  ),
+  row4: (
+    <Row
+      number={4}
+      element="TERRA"
+      title="Corpo / Ações"
+      description="O que fazer com a minha vida?"
+    />
+  ),
 };
 
 export default state;
+
+type Props = {
+  number: number;
+  element: string;
+  title: string;
+  description: string;
+};
+
+function Row({ number, element, title, description }: Props) {
+  return (
+    <div>
+      <b>Nº {number}</b>
+      <div>{element}</div>
+      <div>{title}</div>
+      <div>
+        <small>{description}</small>
+      </div>
+    </div>
+  );
+}
